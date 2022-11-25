928a8c50cd41dacc69e2bfa604e28fc09a4ed4fa
Ting <wisezt@hotmail.com>
2022-11-25 19:43:02 +0800

    Add footballworldCup class

---
+++ src/main/java/com/sportradar/Main.java
@@ -0,0 +1,16 @@
+package com.sportradar;
+
+import com.sportradar.entity.FootballWorldCup;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        FootballWorldCup footballWorldCup = new FootballWorldCup();
+
+        footballWorldCup.gameStart();
+
+
+
+    }
+}
---
+++ src/main/java/com/sportradar/entity/FootballWorldCup.java
@@ -0,0 +1,42 @@
+package com.sportradar.entity;
+
+public class FootballWorldCup {
+
+
+    public void gameStart(){
+
+        System.out.println("The is the Foot World Cup!");
+
+        // initial service
+
+
+        // display welcomeMessage()
+
+
+        // initialAGame();
+
+
+        // Input Home Team Name and Away Team Name
+
+
+        // Display the ScoreBoard
+
+
+        // Input Home Team Score and Away Team Score
+
+
+        // Display the ScoreBoard
+
+
+        // Dispaly the rank in the scoreboard.
+
+
+        // whether continue to play the game
+
+
+
+    }
+
+
+
+}
